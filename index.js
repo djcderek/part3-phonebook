93b6572199ad25db5ecf663eff43399f077c1632
Derek Chan <djcderek@gmail.com>
2023-07-26 22:38:14 -0700

    complete exercise 3.3

--- index.js
+++ index.js
@@ -32,8 +32,12 @@ app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   console.log(id)
   const person = persons.find(person => person.id === id)
-  console.log(person)
-  response.json(person)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.get('/info', (request, response) => {
